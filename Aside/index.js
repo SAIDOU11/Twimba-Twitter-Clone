9f386fc12cf288314d4b3023f2246023a60a71b3
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-17 08:45:29 +0200

    like et retweet colors

--- Aside/index.js
+++ Aside/index.js
@@ -1,7 +1,40 @@
+const galleryContainer = document.querySelector(".gallery-container");
+let isLiked = false;
+let isShared = false;
+
 document.addEventListener("click", function (e) {
-  if (e.target.dataset.shareIcon) {
-    console.log(e.target.dataset.shareIcon);
-  } else if (e.target.dataset.heart) {
-    console.log(e.target.dataset.heart);
+  if (e.target.dataset.heart) {
+    isLiked = !isLiked;
+    render();
+  } else if (e.target.dataset.shareIcon) {
+    isShared = !isShared;
+    render();
   }
 });
+
+function render() {
+  let heartClass = "";
+  let shareClass = "";
+
+  if (isLiked) {
+    heartClass = "liked";
+  }
+  if (isShared) {
+    shareClass = "shared";
+  }
+
+  let imageHtml = `
+  <div id="image-1" class="img-container">
+        <img
+          src="dino2.jpeg"
+          alt="Visite dinosaure. Jurassic Parc"          
+        />
+        <div class="social-icons-container">
+          <i class="fa-solid fa-heart ${heartClass}" data-heart="image-1"></i>
+          <i class="fa-solid fa-share ${shareClass} " data-share-icon="image-1"></i>
+        </div>
+      </div>
+  `;
+  galleryContainer.innerHTML = imageHtml;
+}
+render();
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ tweetBtn.addEventListener("click", () => {
 document.addEventListener("click", (e) => {
   if (e.target.dataset.like) {
     handleLikeClick(e.target.dataset.like);
+  } else if (e.target.dataset.retweet) {
+    handleRetweetClick(e.target.dataset.retweet);
   }
 });
 
@@ -28,24 +30,50 @@ function handleLikeClick(tweetId) {
   render();
 }
 
+function handleRetweetClick(tweetID) {
+  const targetTweetObj = tweetsData.filter((tweet) => {
+    return tweet.uuid === tweetID;
+  })[0];
+
+  if (!targetTweetObj.isRetweeted) {
+    targetTweetObj.retweets++;
+  } else {
+    targetTweetObj.retweets--;
+  }
+  targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted;
+  render();
+}
+
 function getFeedHtml() {
   let feedHtml = "";
 
   tweetsData.forEach((tweet) => {
+    let likeIconClass = "";
+
+    if (tweet.isLiked) {
+      likeIconClass = "liked";
+    }
+
+    let retweetIconClass = "";
+
+    if (tweet.isRetweeted) {
+      retweetIconClass = "limegreen";
+    }
+
     feedHtml += `
     <div class="tweet">
     <div class="tweet-inner">
         <img src="${tweet.profilePic}" class="profile-pic">
         <div>
             <p class="handle">${tweet.handle}</p>
-            <p class="tweet-text">${tweet.tweetText}</p>
+            <p class="tweet-text ${retweetIconClass} ">${tweet.tweetText}</p>
             <div class="tweet-details">
                 <span class="tweet-detail">
                 <i data-reply=${tweet.uuid} class="fa-regular fa-comment-dots"></i>
                 ${tweet.replies.length}
                 </span>
                 <span class="tweet-detail">
-                <i data-like=${tweet.uuid} class="fa-solid fa-heart"></i>
+                <i data-like=${tweet.uuid} class="fa-solid fa-heart ${likeIconClass} "></i>
                 ${tweet.likes}
                 </span>
                 <span class="tweet-detail">
